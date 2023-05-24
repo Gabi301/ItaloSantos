4f569914062eebd00c09eccb6bf0ec90836dfe86
Gabi301 <italouchiha301@gmail.com>
2023-05-24 20:23:39 +0100

    Exercicio tres

---
+++ ItaloSantos.py
@@ -0,0 +1,16 @@
+def F2Celsius(conversao_fahrenheit):
+    conversao_celsius = (5/9) * (conversao_fahrenheit - 32)
+    return conversao_celsius
+
+conversao_fahrenheit = 50
+conversao_celsius = F2Celsius(conversao_fahrenheit)
+print(conversao_celsius)
+
+
+
+```python
+def say_hi():
+    print('Hi, my name is Ítalo Santos.')
+
+__version__ = '0.1'
+```
